55c41cc666edf3749e2058b27a9346ad73dc8255
codynguyen <cody.haind@gmail.com>
2021-09-16 17:59:25 +0700

    implement browser .connect() method

--- src/api.js
+++ src/api.js
@@ -3,7 +3,15 @@ const { create } = require('apisauce');
 const jwt = require('jsonwebtoken');
 const { API_URL, CONNECT_URL } = require('./constants');
 const createResources = require('./resources');
-const { isBrowser, isNode, isReactNative } = require('./utils');
+const {
+  isBrowser,
+  isNode,
+  isReactNative,
+  appendIframe,
+} = require('./utils');
+
+const CONNECTION_SUCCESS = 'CONNECTION_SUCCESS';
+const CONNECTION_FAILURE = 'CONNECTION_FAILURE';
 
 class API {
   constructor(config) {
@@ -20,7 +28,6 @@ class API {
     this.isNode = isNode();
     this.isReactNative = isReactNative();
     this.isClient = this.isBrowser || this.isReactNative;
-    this._token = {};
 
     // TODO add an error code for each SDK error
     if (!clientId || typeof clientId !== 'string') {
@@ -40,6 +47,11 @@ class API {
       };
       this.authApi = create({ headers: this.config.auth.headers });
     }
+
+    this._token = {};
+    this._onConnectorMessage = this._onMessage.bind(this, 'connector');
+    this._isConnecting = false;
+    this._isWaitingForConnector = false;
   }
 
   init() {
@@ -183,6 +195,164 @@ class API {
     const url = provider ? `${connectURL}/connect/${provider}` : `${connectURL}/connect`;
 
     return `${url}?${queryString}`;
+  }
+
+  connect(options = {}) {
+    this._connect(options);
+
+    return this; // return the instance so we can chain the callbacks
+  }
+
+  onConnection(callback) {
+    if (typeof callback !== 'function') throw new Error('Callback must be a function.');
+    this._onConnection = callback.bind(this);
+
+    return this;
+  }
+
+  onError(callback) {
+    if (typeof callback !== 'function') throw new Error('Callback must be a function.');
+    this._onError = callback.bind(this);
+
+    return this;
+  }
+
+  onEvent(callback) {
+    if (typeof callback !== 'function') throw new Error('Callback must be a function.');
+    this._onEvent = callback.bind(this);
+
+    return this;
+  }
+
+  async _connect(options = {}) {
+    try {
+      this._isConnecting = true;
+      const { provider } = options;
+      const url = await this.getConnectUrl({ provider });
+
+      this.iframe = appendIframe('vezgo-connect-widget');
+      this.connector = window.open(url, this.iframe.name);
+      this.connector.focus();
+
+      this._isWaitingForConnector = true;
+      this._watchConnector();
+    } catch (error) {
+      this._triggerCallback(CONNECTION_FAILURE, { error_type: 500, message: 'Connection refused' });
+    }
+  }
+
+  _onMessage(emitter, { origin, data }) {
+    let result = data;
+    try {
+      result = JSON.parse(data);
+    } catch (err) {
+      // Do nothing
+    }
+
+    if (result.vezgo) {
+      if (origin !== this.connectUrl && !(/\.vezgo\./).test(new URL(origin).hostname)) {
+        throw new Error(`Unauthorized attempt to call SDK from origin: ${origin}`);
+      }
+
+      switch (result.eventName) {
+        case 'connectSuccess': {
+          if (emitter === 'connector') {
+            this._isWaitingForConnector = false;
+          }
+
+          if (result.account) {
+            // Set accountId to the vezgo instance for zabo compatibility?
+          }
+
+          this._triggerCallback(CONNECTION_SUCCESS, result.account);
+          break;
+        }
+
+        case 'connectError': {
+          if (emitter === 'connector') {
+            this._isWaitingForConnector = false;
+          }
+
+          this._triggerCallback(CONNECTION_FAILURE, result.error);
+          break;
+        }
+
+        case 'connectClose': {
+          this._closeConnector();
+          break;
+        }
+
+        default: {
+          if (this._onEvent) {
+            // List of zabo events for compatibility https://zabo.com/docs/#zabo-connect-callbacks
+            this._onEvent(result.eventName, result.metadata || {});
+          }
+        }
+      }
+    }
+  }
+
+  _watchConnector() {
+    this._setListeners();
+
+    // Connector timeout (10 minutes). This is the absolute max timeout for the connector
+    const connectorTimeout = setTimeout(() => {
+      this._closeConnector();
+      this._triggerCallback(CONNECTION_FAILURE, { error_type: 400, message: 'Connection timeout' });
+    }, 10 * 60 * 1000);
+
+    // Watch interval. This watches for connector status (whether closed by user or successfully
+    // finished) to clean it up.
+    const watchInterval = setInterval(() => {
+      if (this._isWaitingForConnector) {
+        if (this.connector.closed) {
+          this._closeConnector(); // Ensure that the connector has been destroyed
+          this._triggerCallback(CONNECTION_FAILURE, { error_type: 400, message: 'Connection closed' });
+        }
+      } else {
+        this._removeListeners();
+        clearInterval(watchInterval);
+        clearTimeout(connectorTimeout);
+
+        this._closeConnector();
+      }
+    }, 1000);
+  }
+
+  _closeConnector() {
+    this._isWaitingForConnector = false;
+
+    if (!this.connector.closed) {
+      this.connector.close();
+    }
+
+    if (this.iframe) {
+      this.iframe.style.display = 'none';
+      this.iframe.src = '';
+    }
+  }
+
+  _triggerCallback(type, data) {
+    if (this._isConnecting) {
+      this._isConnecting = false;
+
+      if (type === CONNECTION_SUCCESS && this._onConnection) {
+        this._onConnection(data);
+      }
+
+      if (type === CONNECTION_FAILURE && this._onError) {
+        this._onError(data);
+      }
+    }
+  }
+
+  _setListeners() {
+    // Listen to postMessage (iframe/browser flow)
+    window.addEventListener('message', this._onConnectorMessage, false);
+  }
+
+  _removeListeners() {
+    window.removeEventListener('message', this._onConnectorMessage, false);
   }
 }
 
--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-/* global window,navigator */
+/* global window,navigator,document */
 
 const isBrowser = () => typeof window !== 'undefined' && Object.prototype.toString.call(window) === '[object Window]';
 
@@ -6,8 +6,29 @@ const isNode = () => typeof global !== 'undefined' && Object.prototype.toString.
 
 const isReactNative = () => typeof navigator !== 'undefined' && navigator.product === 'ReactNative';
 
+function appendIframe(name) {
+  let iframe = document.getElementsByName(name)[0];
+
+  if (!iframe) {
+    iframe = document.createElement('iframe');
+    iframe.setAttribute('style', 'position:fixed; top:0; left:0; right:0; bottom:0; z-index:2147483647;');
+    iframe.style.width = '100%';
+    iframe.style.height = '100%';
+    iframe.frameBorder = 0;
+    iframe.allow = 'usb *; hid *';
+    iframe.name = name;
+
+    document.body.appendChild(iframe);
+  }
+
+  iframe.style.display = 'block';
+
+  return iframe;
+}
+
 module.exports = {
   isBrowser,
   isNode,
   isReactNative,
+  appendIframe,
 };
