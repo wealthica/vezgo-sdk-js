3a5c15d5f043f8f3743b7a7186566f0271acc8fc
codynguyen <cody.haind@gmail.com>
2021-10-08 17:38:28 +0700

    add tests for Accounts resource helpers

--- src/resources/accounts.js
+++ src/resources/accounts.js
@@ -11,7 +11,7 @@ class Accounts {
   }
 
   async getOne(id) {
-    if (!id) throw new Error('Please provide a valid Vezgo account id.');
+    if (!id || typeof id !== 'string') throw new Error('Please provide a valid Vezgo account id.');
 
     const response = await this.api.get(`/accounts/${id}`);
     if (!response.ok) throw response.originalError;
@@ -20,7 +20,7 @@ class Accounts {
   }
 
   async remove(id) {
-    if (!id) throw new Error('Please provide a valid Vezgo account id.');
+    if (!id || typeof id !== 'string') throw new Error('Please provide a valid Vezgo account id.');
 
     const response = await this.api.delete(`/accounts/${id}`);
     if (!response.ok) throw response.originalError;
