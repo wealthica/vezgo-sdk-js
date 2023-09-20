fac6ac729b5869c1ca71c175dc360ccb3c755de2
Oleg <oleh.vysotskyi@gmail.com>
2023-09-20 13:02:15 +0400

    parse providerCategories as array

--- src/api.js
+++ src/api.js
@@ -197,7 +197,8 @@ class API {
       origin,
       sync_nfts: syncNfts === false ? false : undefined, // only pass if it is false
       demo: this.config.demo ? true : undefined,
-      provider_categories: providerCategories || undefined,
+      // 'provider' param in priority, skip 'provider_categories' param if 'provider' is set
+      provider_categories: !provider && Array.isArray(providerCategories) && providerCategories.length ? providerCategories.join(',') : undefined,
       // 'provider' param in priority, skip 'providers' param if 'provider' is set
       providers: !provider && Array.isArray(providers) && providers.length ? providers.join(',') : undefined,
       theme: ['light', 'dark'].includes(theme) ? theme : 'light',
